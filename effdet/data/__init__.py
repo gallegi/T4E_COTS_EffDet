261a70b3657f92e3b860d0a679995e0a7dfb9803
Nguyen The Nam <namnguyen61031@gmail.com>
2022-01-15 23:23:18 +0700

    Declare create_cots_dataset on __init__.py

--- effdet/data/__init__.py
+++ effdet/data/__init__.py
@@ -1,4 +1,4 @@
-from .dataset_factory import create_dataset
+from .dataset_factory import create_dataset, create_cots_dataset
 from .dataset import DetectionDatset, COTSDetectionDatset, SkipSubset
 from .input_config import resolve_input_config
 from .loader import create_loader
